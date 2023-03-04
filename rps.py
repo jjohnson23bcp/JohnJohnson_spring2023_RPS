8f57c3b24d0c89943cd925de0fd01e55e39acc2e
jjohnson23bcp <126900779+jjohnson23bcp@users.noreply.github.com>
2023-03-03 20:01:49 -0800

    working game
    Has a few kinks I'm unsure how to work out

--- rps.py
+++ rps.py
@@ -35,54 +35,44 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Rock, Paper, Scissors...")
 # defines the class game clock, makes it ready for use
 clock = pg.time.Clock()
+# rock images
 # defining rock image - shows where to get the image
 rock_image = pg.image.load(os.path.join(game_folder, 'rock.jpg')).convert()
 # storing where the pixel are and the dimensions, allows us to access and change them
 rock_image_rect = rock_image.get_rect()
 # rock image the cpu is going to use
 cpu_rock_image_rect = rock_image.get_rect()
-# hides the rock image for the cpu off the screen ready to be used
-cpu_rock_image_rect.y = 2000
+# paper images
 paper_image = pg.image.load(os.path.join(game_folder, 'paper.jpg')).convert()
 paper_image_rect = paper_image.get_rect()
 # adjusts where the image will appear on the screen
 paper_image_rect.x = 220
 cpu_paper_image_rect = paper_image.get_rect()
-cpu_paper_image_rect.x = 220
-cpu_paper_image_rect.y = 2000
+# scissor images
 scissors_image = pg.image.load(os.path.join(game_folder, 'scissors.jpg')).convert()
 scissors_image_rect = scissors_image.get_rect()
 scissors_image_rect.x = 400
 cpu_scissors_image_rect = scissors_image.get_rect()
-cpu_scissors_image_rect.x = 400
-cpu_scissors_image_rect.y = 2000
+# win image
+win_image = pg.image.load(os.path.join(game_folder, 'win.jpg')).convert()
+win_image_rect = win_image.get_rect()
+# lose image
+lose_image = pg.image.load(os.path.join(game_folder, 'lose.jpg')).convert()
+lose_image_rect = lose_image.get_rect()
+# tie image
+tie_image = pg.image.load(os.path.join(game_folder, 'tie.jpg')).convert()
+tie_image_rect = tie_image.get_rect()
 # defining the variable choices as a list from 0-2 
 choices = ["Rock", "Paper", "Scissors"]
 # function cpu_randchoice that tells the computer to randomly choose rock paper or scissors
 def cpu_randchoice():
+    # makes the cpu variable usable outside of the definition loop
     global cpu
     # defining the variable cpu as the list choices which are randomly picked
     cpu = choices[randint(0,2)]
+    sleep(1)
     # displays the string in the terminal
-    sleep(1)
     print("Computer randomly chose", cpu)
-# function compare which compares the user and computer choices, decides who wins / what the outcome is
-def compare(user, cpu):
-    sleep(1)
-    # if conditions for deciding if the user wins 
-    # .capitalize() makes the first letter of the user's choice uppercase
-    if user.capitalize() == "Rock" and cpu == "Scissors":
-        print("Win")
-    elif user.capitalize() == "Scissors" and cpu == "Paper":
-        print("Win")
-    elif user.capitalize() == "Paper" and cpu == "Rock":
-        print ("Win")
-    # conditions to tie
-    elif user.capitalize() == cpu:
-        print ("Tie")
-    # if none of the other conditions are met, the user loses
-    else:
-        print("Lose")
 # turns on
 running = True
 # starts while loop
@@ -98,88 +88,116 @@ while running:
         # the event of when the mouse button is released
         if event.type == pg.MOUSEBUTTONUP:
             # finds the mouse position and stores it 
-            mouse_coords = pg.mouse.get_pos()
-            # if rock image is selected, print Rock in the terminal 
+            mouse_coords = pg.mouse.get_pos() 
             if rock_image_rect.collidepoint(mouse_coords):
+                # if rock image is selected, print Rock in the terminal
                 print("Rock")
+                # set user equal to rock to be used in the comparitive statements
                 user = "Rock"
-                cpu = cpu_randchoice()
-                # if gameover is false, make gameover true, else gameover will stay false
+                # calling the function for the computer to randomly choose
+                cpu_randchoice()
+                # will make GAMEOVER true if it is not true, otherwise it will become false
                 if GAMEOVER == False:
                     GAMEOVER = True
                 else:
                     GAMEOVER = False
+            # if user chooses paper
             elif paper_image_rect.collidepoint(mouse_coords):
                 print("Paper")
                 user = "Paper"
-                cpu = cpu_randchoice()
+                cpu_randchoice()
                 if GAMEOVER == False:
                     GAMEOVER = True
                 else:
                     GAMEOVER = False
+            # if user chooses scissors
             elif scissors_image_rect.collidepoint(mouse_coords):
                 print("Scissors")
                 user = "Scissors"
-                cpu = cpu_randchoice()
+                cpu_randchoice() 
                 if GAMEOVER == False:
                     GAMEOVER = True
                 else:
-                    GAMEOVER = False       
+                    GAMEOVER = False     
             else:
                 print("Select Rock Paper or Scissors")
     ################### update ####################
    
     ################### draw ################
     # fills the screen in with black
-    screen.fill(BLACK)  
-    # if game still running 
+    screen.fill(BLACK)      
+    #  if the rock image is select, draw only the rock image, the other images will leave the screen
     if not GAMEOVER:
         # drawing the rock image on the screen
         screen.blit(rock_image, rock_image_rect)
         screen.blit(paper_image, paper_image_rect)
         screen.blit(scissors_image, scissors_image_rect)
-    #  if the rock image is select, draw only the rock image, the other images will leave the screen
+    # different paths for each option the user has
     elif user == "Rock":
+        # different options the computer has when it randomly chooses
         if cpu == "Rock":
-            cpu_rock_image_rect.y = 100
-            screen.blit(rock_image, rock_image_rect)
+            # calls the cpu rock image onto the screen
+            cpu_rock_image_rect.y = 200
+            # draws only the rock image and cpu rock image
             screen.blit(rock_image, cpu_rock_image_rect)
+            screen.blit(rock_image, rock_image_rect)
+            # delays next line of code
+            sleep(2)
+            # draws the result of the matchup
+            screen.blit(tie_image, tie_image_rect)
         if cpu == "Paper":
-            cpu_paper_image_rect.y = 100
-            screen.blit(rock_image, rock_image_rect)
+            cpu_paper_image_rect.y = 200
             screen.blit(paper_image, cpu_paper_image_rect)
+            screen.blit(rock_image, rock_image_rect)  
+            sleep(2)
+            screen.blit(lose_image, lose_image_rect)
         if cpu == "Scissors":
-            cpu_scissors_image_rect.y = 100
-            screen.blit(rock_image, rock_image_rect)
+            cpu_scissors_image_rect.y = 200
             screen.blit(scissors_image, cpu_scissors_image_rect)
+            screen.blit(rock_image, rock_image_rect)
+            sleep(2)
+            screen.blit(win_image, win_image_rect)
+    # paper paths
     elif user == "Paper":
         if cpu == "Rock":
-            cpu_rock_image_rect.y = 100
+            cpu_rock_image_rect.y = 200
             screen.blit(paper_image, paper_image_rect)
             screen.blit(rock_image, cpu_rock_image_rect)
+            sleep(2)
+            screen.blit(win_image, win_image_rect)
         if cpu == "Paper":
-            cpu_paper_image_rect.y = 100
+            cpu_paper_image_rect.y = 200
             screen.blit(paper_image, paper_image_rect)
             screen.blit(paper_image, cpu_paper_image_rect)
+            sleep(2)
+            screen.blit(tie_image, tie_image_rect)
         if cpu == "Scissors":
-            cpu_scissors_image_rect.y = 100
+            cpu_scissors_image_rect.y = 200
             screen.blit(paper_image, paper_image_rect)
             screen.blit(scissors_image, cpu_scissors_image_rect)
+            sleep(2)
+            screen.blit(lose_image, lose_image_rect)
+    # scissor paths
     elif user == "Scissors":
         if cpu == "Rock":
-            cpu_rock_image_rect.y = 100
+            cpu_rock_image_rect.y = 200
             screen.blit(scissors_image, scissors_image_rect)
             screen.blit(rock_image, cpu_rock_image_rect)
-        if cpu == "Paper":
+            sleep(2)
+            screen.blit(lose_image, lose_image_rect)
+        elif cpu == "Paper":
             cpu_paper_image_rect.y = 100
             screen.blit(scissors_image, scissors_image_rect)
             screen.blit(paper_image, cpu_paper_image_rect)
-        if cpu == "Scissors":
-            cpu_scissors_image_rect.y = 100
+            sleep(2)
+            screen.blit(win_image, win_image_rect)
+        elif cpu == "Scissors":
+            cpu_scissors_image_rect.y = 200
             screen.blit(scissors_image, scissors_image_rect)
             screen.blit(scissors_image, cpu_scissors_image_rect)
-    else: 
-        pass
+            sleep(2)
+            screen.blit(tie_image, tie_image_rect)
+    # game display is modified
     pg.display.flip()
 # quits pygame
 pg.quit()
